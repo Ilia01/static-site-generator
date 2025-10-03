c3021578f21af4fde312e5fdd05e53bb65d7cc87
Ilia01 <ilia.goginashvili.1@btu.edu.ge>
2025-10-04 02:50:08 +0400

    Add text processing pipeline and HTML conversion
    - Implement text_to_textnodes main processing pipeline
    - Add text_node_to_html_node conversion mapping
    - Create CONVERSION_MAP for TextNode to HTMLNode transformation
    - Support all text types: text, bold, italic, code, links, images

---
+++ src/utils/node_to_html.py
@@ -0,0 +1,22 @@
+from src.leafnode import LeafNode
+from src.textnode import TextType, TextNode
+
+CONVERSION_MAP = {
+    TextType.TEXT: lambda node: LeafNode(tag=None, value=node.text),
+    TextType.BOLD: lambda node: LeafNode(tag="b", value=node.text),
+    TextType.ITALIC: lambda node: LeafNode(tag="i", value=node.text),
+    TextType.CODE: lambda node: LeafNode(tag="code", value=node.text),
+    TextType.LINK: lambda node: LeafNode(
+        tag="a", value=node.text, props={"href": node.url}
+    ),
+    TextType.IMAGE: lambda node: LeafNode(
+        tag="img", value="", props={"src": node.url, "alt": node.text}
+    ),
+}
+
+
+def text_node_to_html_node(text_node):
+    conversion_func = CONVERSION_MAP.get(text_node.text_type)
+    if conversion_func:
+        return conversion_func(text_node)
+    raise ValueError(f"Unknown text type: {text_node.text_type}")
---
+++ src/utils/text_textnodes.py
@@ -0,0 +1,19 @@
+from src.utils.split_nodes import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+)
+from src.textnode import TextNode, TextType
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text=text, text_type=TextType.TEXT)]
+
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+
+    return nodes
